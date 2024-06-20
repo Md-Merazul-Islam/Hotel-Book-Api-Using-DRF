df1ca294cf47542944fcbb82b38631d977c854c6
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-06-20 17:02:38 +0600

    cros truested

--- hotel_booking_project/settings.py
+++ hotel_booking_project/settings.py
@@ -11,22 +11,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://blueskybooking.onrender.com",
-    "http://localhost:3000",
+    'http://*',
+    'https://*',
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://blueskybooking.onrender.com',
     'https://*.127.0.0.1',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000',
 ]
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-]
+
+
 
 CORS_ALLOW_HEADERS = [
     "accept",
