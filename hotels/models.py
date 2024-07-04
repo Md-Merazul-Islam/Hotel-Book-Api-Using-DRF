a4ad1513320758861c2d5d6711f8c3bddce960e1
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-07-04 11:07:58 +0600

    update

--- hotels/models.py
+++ hotels/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 
+
 class District(models.Model):
     district_name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
@@ -14,6 +15,7 @@ class District(models.Model):
             self.slug = slugify(self.district_name)
         super().save(*args, **kwargs)
 
+
 class Hotel(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
@@ -21,13 +23,12 @@ class Hotel(models.Model):
     description = models.TextField()
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     available_room = models.IntegerField(default=0)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='hotels')
+    district = models.ForeignKey(
+        District, on_delete=models.CASCADE, related_name='hotels')
 
     def __str__(self):
         return f'{self.name} in {self.district.district_name}'
-    
-from django.db import models
-from django.contrib.auth.models import User
+
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -40,6 +41,7 @@ class Booking(models.Model):
     def __str__(self):
         return f'Booking by {self.user.username} at {self.hotel.name} from {self.start_date} to {self.end_date}'
 
+
 class Review(models.Model):
     STAR_CHOICES = [
         ('⭐', '⭐'),
@@ -48,8 +50,10 @@ class Review(models.Model):
         ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
         ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
     ]
-    hotel = models.ForeignKey(Hotel,on_delete=models.SET_NULL, null=True, blank=True,related_name='reviews',)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    hotel = models.ForeignKey(
+        Hotel, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviews',)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     rating = models.CharField(choices=STAR_CHOICES, max_length=10)
@@ -62,3 +66,11 @@ class Review(models.Model):
             return f'Review by {self.user.username} for {self.hotel.name}'
         else:
             return f'Review for {self.hotel.name} (User not specified)'
+
+
+class ContactUsModels(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+
+
