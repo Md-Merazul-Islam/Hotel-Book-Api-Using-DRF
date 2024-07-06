ddba368adffc4f89b6a5cf78b59664f1196ec03b
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-07-06 08:59:24 +0600

    update isadmin

--- hotels/permissions.py
+++ hotels/permissions.py
@@ -20,5 +20,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Only allow admin users to create, update, or delete objects
         return request.user and request.user.is_staff
